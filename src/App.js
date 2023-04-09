c772d949c5c7b36aee54266504638e63adc8282a
Luiz <luiz7tenshi@gmail.com>
2023-04-09 12:12:14 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,89 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
 
+
+import {useFetch} from './hooks/useFetch';
+
 function App() {
+
+  const url = 'http://localhost:3000/products'
+
+  // const [products, setProducts] = useState([])
+
+  // 4.1 - custom hook
+  const {data: items, httpConfig, loading, error} = useFetch(url)
+
+  const [name, setName] = useState('')
+  const [price, setPrice] = useState('')
+
+
+  // 1 - resgatando os dados sem o custom hook useFetch
+  // const myFunction = async () => {
+  //   const res = await fetch(url)
+  //   const data = await res.json()
+  //   setProducts(data)
+  //   console.log(products)
+  // };
+  // useEffect(() => { myFunction(); }, []);
+
+  //2 - add de produtos
+  const handleSubmit = async (e) => {
+
+    e.preventDefault()
+
+    const product = {
+      name,
+      price,
+    }
+
+    // const res = await fetch(url, {
+    //   method: 'POST',
+    //   headers: {
+    //     'Content-Type': 'application/json'
+    //   },
+    //   body: JSON.stringify(product)
+    // })
+
+    // const addedProduct = await res.json()
+
+    // //3 - carregamento dinamico
+    // setProducts((prevProducts) => [...prevProducts, addedProduct])
+
+    // 5 - refatorando post
+
+    httpConfig(product, 'POST')
+    setName('')
+    setPrice('')
+  }
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>Lista de produtos</h2>
+      {/* 6- loading */}
+      { loading && <p>Carregando items...</p>}
+      { error && {error}}
+      <ul>
+        {items && items.map((product) => (
+          <li key={product.id}>{product.name} - R$ {product.price}</li>
+        ))}
+      </ul>
+
+      <div className="add-product">
+        <form onSubmit={handleSubmit}>
+          <label>
+            Nome:
+            <input type="text" name='name' onChange={(e) => setName(e.target.value)} />
+          </label>
+          <label>
+            Preço:
+            <input type="text" name='price' onChange={(e) => setPrice(e.target.value)} />
+          </label>
+          
+          {/* 7 - state de loading no post */}
+          {!loading && <input type="submit" value="Criar" />}
+        </form>
+      </div>
     </div>
   );
 }
---
+++ src/hooks/useFetch.js
@@ -0,0 +1,80 @@
+import { useState, useEffect } from "react";
+
+// 4- custom hook
+
+export const useFetch = (url) => {
+    const [data, setData] = useState(null)
+
+
+    // 5 - refaturando post
+
+    const [config, setConfig] = useState(null)
+    const [method, setMethod] = useState(null)
+    const [callFetch, setCallFetch] = useState(false)
+
+
+    // 6- loading
+
+    const [loading, setLoading] = useState(false)
+
+    // 7- tratando erros
+    const [error, setError] = useState(null)
+
+    const httpConfig = (data, method) => {
+        if (method === 'POST') {
+            setConfig({
+                method,
+                headers: {
+                    'Content-type': 'application/json'
+                },
+                body: JSON.stringify(data)
+            })
+        }
+        setMethod(method)
+    }
+
+    useEffect(() => {
+        const fetchData = async () => {
+
+            //6 - loading
+            setLoading(true)
+
+            try {
+                const res = await fetch(url)
+
+                const json = await res.json()
+
+                setData(json)
+
+                setLoading(false)
+
+            } catch (error) {
+                console.log(error.message)
+                setError('Houve um erro ao carregar os dados!')
+            }
+        }
+
+        fetchData()
+    }, [url, callFetch])
+
+    // 5 - refaturando o post
+
+    useEffect(() => {
+        const httpRequest = async () => {
+            if (method === 'POST') {
+                let fetchOptions = [url, config]
+
+                const res = await fetch(...fetchOptions)
+
+                const json = await res.json();
+
+                setCallFetch(json)
+            }
+        }
+
+        httpRequest();
+
+    }, [config, method, url])
+
+    return { data, httpConfig, loading, error }
+}
